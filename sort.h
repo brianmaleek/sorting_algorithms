357344463cd97d04e5b9bbadf502bb99ba7abc6c
brianmaleek <maleekmalik@gmail.com>
2023-07-26 00:48:16 +0300

    chmod unintended changes

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -27,4 +28,3 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
-```
